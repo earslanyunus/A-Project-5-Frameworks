7216fcfc9dc1ea4fc9ad06019a8b5c09b7693272
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-07-06 23:10:49 +0300

    added footer component to main layout

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/common/Navbar"
+import Footer from "./components/common/Footer"
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Outlet/>
+      <Footer/>
     </>
   )
 }
