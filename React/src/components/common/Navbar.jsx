53db77ac474282d42f4319eff5252152150a578e
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-07-06 20:13:51 +0300

    finished navbar component

--- React/src/components/common/Navbar.jsx
+++ React/src/components/common/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () =>{
     return(
         <div className="navbar bg-base-300">
@@ -7,20 +9,20 @@ const Navbar = () =>{
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Movies</a></li>
+        <li><Link to={'movies'}>Movies</Link></li>
 
-        <li><a>Series</a></li>
-        <li><a>Actors</a></li>
+        <li><Link to={'series'}>Series</Link></li>
+        <li><Link to={'actors'}>Actors</Link></li>
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl">A-Project</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-    <li><a>Movies</a></li>
+    <li><Link to={'movies'}>Movies</Link></li>
 
-<li><a>Series</a></li>
-<li><a>Actors</a></li>
+<li><Link to={'series'}>Series</Link></li>
+<li><Link to={'actors'}>Actors</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
@@ -32,12 +34,11 @@ const Navbar = () =>{
       </label>
       <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-52">
         <li>
-          <a className="justify-between">
+          <Link to={'profile'} className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
-        <li><a>Settings</a></li>
         <li><a>Logout</a></li>
       </ul>
     </div>  </div>
