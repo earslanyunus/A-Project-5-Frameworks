72be49b9052d1de0391878e6ef36630dddf1dddc
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-07-12 01:36:57 +0300

    refactored layout system

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,15 +1,36 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "./components/common/Navbar"
-import Footer from "./components/common/Footer"
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+import { useEffect, useState } from "react";
+import { getPopularMovies, getPopularPeople, getPopularSeries } from "./utils/tmdb";
+import { SwiperPart } from "./components/SwiperPart";
 
 const App = () => {
-  return (
-    <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
-    </>
-  )
-}
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [popularPeoples, setPopularPeoples] = useState([]);
+  const [popularSeries, setPopularSeries] = useState([]);
+  useEffect(() => {
+    getPopularMovies().then((data) => {
+      setPopularMovies(data);
+    });
+    getPopularPeople().then((data) => {
+      setPopularPeoples(data);
+    });
+    getPopularSeries().then(data=>{
+      setPopularSeries(data)
+    })
+  }, []);
 
-export default App
+  return (
+    <div className=" min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex flex-col mt-8">
+        <SwiperPart head={"Popular Movies"} movies={popularMovies} />
+        <SwiperPart head={"Popular Actors"} movies={popularPeoples} />
+        <SwiperPart head={"Popular Series"} movies={popularSeries} />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MoviesPage from './view/MoviesPage.jsx'
+import DetailLayout from './view/DetailLayout.jsx'
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+   
+  },
+  {
+    path:'detail',
+    element: <DetailLayout/>,
     children:[
       {
-
+        path:'movies',
+        element: <MoviesPage/>,
       }
     ]
   }
