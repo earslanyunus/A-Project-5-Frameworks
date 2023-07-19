18e2bad96e1ce5ae597713cfb4d6a9e7c1d8d2bf
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-07-19 20:35:18 +0300

    created svelte version

---
+++ Svelte/src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let count2 = 5
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+count2 is {count2}
+
