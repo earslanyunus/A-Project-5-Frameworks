fe7293452fda57a68efbd3d045537309ee3401e0
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-09-14 00:55:49 +0300

    detail person page is finished

--- React/src/view/Detail_person.jsx
+++ React/src/view/Detail_person.jsx
@@ -1,29 +1,91 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {
-    getMovieCredits,
-    getMovieDetails,
     getPersonDetails,
     getPersonMovies,
     getPersonSeries,
-    getSimilarMovies
 } from "../utils/tmdb.js";
 import {useParams} from "react-router-dom";
+import { SwiperPart } from "../components/SwiperPart.jsx";
+const imagePath = 'https://image.tmdb.org/t/p/original'
 
 const Detail_person = () => {
     const {id} = useParams()
+    
+    const [personDetail,setPersonDetail] = useState([])
+    const [personMovies,setPersonMovies] = useState([])
+    const [personSeries,setPersonSeries] = useState([])
     useEffect(()=>{
-        const getData = async ()=>{
-            await getPersonDetails(id)
-            await getPersonMovies(id)
-            await getPersonSeries(id)
-        }
-        getData()
+       getPersonDetails(id)
+       .then(elm=>{
+            setPersonDetail(elm)
+       })
+       getPersonMovies(id)
+       .then(elm=>{
+        
+       elm =  elm.crew.reduce((accumulator, current) => {
+            let exists = accumulator.find(item => {
+                return item.id === current.id;
+            });
+            if (!exists) {
+                accumulator = accumulator.concat(current);
+            }
+            return accumulator;
+        }, []);
+        elm.sort((a, b) => {
+            if (a.popularity > b.popularity) {
+                return -1
+            }
+            if (a.popularity < b.popularity) {
+                return 1
+            }
+            return 0
+        })
+            setPersonMovies(elm)
+       })
 
+       getPersonSeries(id)
+       .then(elm=>{
+        elm =  elm.crew.reduce((accumulator, current) => {
+            let exists = accumulator.find(item => {
+                return item.id === current.id;
+            });
+            if (!exists) {
+                accumulator = accumulator.concat(current);
+            }
+            return accumulator;
+        }, []);
+        elm.sort((a, b) => {
+            if (a.popularity > b.popularity) {
+                return -1
+            }
+            if (a.popularity < b.popularity) {
+                return 1
+            }
+            return 0
+        })
+            setPersonSeries(elm)
+       })
     },[])
 
 
     return(
-        <p>Detail Person</p>
+        <main className="container">
+            <div className="flex mt-4 mb-4">
+            <img className={'w-1/5 rounded '} src={imagePath + personDetail.profile_path} alt="" />
+            <div className="ms-12">
+                    <p className="text-5xl upp">{personDetail?.name}</p>
+                    <div className="badge badge-outline badge-sm">Born on {new Date(personDetail.birthday).toLocaleDateString('tr')}</div>
+                    <p className="mt-4 mb-4">{personDetail?.biography}</p>
+
+                      
+
+                      
+                </div>
+            </div>
+            {personMovies.length>0 && <SwiperPart head={'Movies'} cards={personMovies} type={'movie'}/>
+        }
+            {personSeries.length>0 && <SwiperPart head={'Series'} cards={personSeries} type={'series'}/>}
+        </main>
     )
 
 }
