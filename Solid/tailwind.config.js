2b7d43cdf025ea27f88985d2d9ca2c71849da7ec
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-10-24 23:22:57 +0300

    tailwind config is changed for centering container

--- Solid/tailwind.config.js
+++ Solid/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      container:{
+        center:true
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
