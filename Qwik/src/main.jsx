1d07529fa920b9ce162e330948895786a373a90c
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-10-27 22:43:43 +0300

    qwik is removed

--- Qwik/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import '@builder.io/qwik/qwikloader.js'
-
-import { render } from '@builder.io/qwik'
-import { App } from './app.jsx'
-import './index.css'
-import page from 'page'
-console.log(page)
-
-render(document.getElementById('app'), page())
