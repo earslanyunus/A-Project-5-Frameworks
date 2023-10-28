901f0e8c285069669cdc05d547ebc18f6aef0cdd
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-10-28 23:50:11 +0300

    vue part is started to creating

---
+++ Vue/src/components/SwiperPerView.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+  import { register } from 'swiper/element/bundle';
+  import "swiper/css";
+import { ref, watchEffect } from "vue";
+import { RouterLink } from "vue-router";
+  register();
+  const props = defineProps(['movies'])
+  watchEffect(()=>{
+    console.log(props.movies);
+  })
+  const controlTypeofData = (data) => {
+    if (data.poster_path) {
+      if (data.first_air_date) {
+        return `detail/tv/${data.id}`;
+      } else {
+        return `detail/movie/${data.id}`;
+      }
+    } else {
+      return `detail/person/${data.id}`;
+    }
+  };
+</script>
+<template>
+<swiper-container
+  class="container mx-auto"
+  space-between='48'
+  slides-per-view='5'
+>
+
+  <swiper-slide class="h-full pb-4" v-for="movie in movies">
+    <RouterLink :to='controlTypeofData(movie)'>
+      <div class="card w-56 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            class="object-contain w-[100%] h-auto"
+            :src='`https://image.tmdb.org/t/p/original/${movie.poster_path || movie.profile_path}`'
+          />
+        </figure>
+        
+        <div class="card-body h-full">
+          <p class="card-title whitespace-nowrap text-ellipsis overflow-hidden block">{{movie.title || movie.name}}</p>
+        </div>
+      </div>
+    </RouterLink>
+  </swiper-slide>
+</swiper-container>
+
+</template>
---
+++ Vue/src/utils/tmdb.js
@@ -0,0 +1,156 @@
+const baseUrl = 'https://api.themoviedb.org/3/'
+export const getPopularMovies = async () => {
+  try {
+    const response = await fetch(
+      `${baseUrl}movie/popular?api_key=${
+        import.meta.env.VITE_MOVIEDB_API_KEY
+      }`
+    );
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getPopularPeople = async () => {
+  try {
+    const response = await fetch(
+      `${baseUrl}person/popular?api_key=${
+        import.meta.env.VITE_MOVIEDB_API_KEY
+      }`
+    );
+    const data = await response.json();
+    return data.results
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getPopularSeries = async () => {
+    try {
+      const response = await fetch(
+        `${baseUrl}tv/top_rated?api_key=${
+          import.meta.env.VITE_MOVIEDB_API_KEY
+        }`
+      );
+      const data = await response.json();
+      return data.results
+    } catch (error) {
+      throw error;
+    }
+  };
+
+export const getMovieDetails = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}movie/${id}?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+export const getMovieCredits = async (movieid)=>{
+  try {
+    const response = await  fetch(`${baseUrl}movie/${movieid}/credits?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`)
+    const data = await response.json()
+    return data
+  }
+  catch (e) {
+    throw e
+  }
+
+}
+
+export const getSimilarMovies = async (movieid)=>{
+  try{
+    const data = await fetch(`${baseUrl}movie/${movieid}/similar?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`)
+    const response = await data.json()
+    return response
+  }
+  catch (e) {
+    throw e
+  }
+}
+
+export const getPersonDetails = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}person/${id}?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+
+export const getPersonMovies = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}person/${id}/movie_credits?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+export const getPersonSeries = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}person/${id}/tv_credits?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+
+export const getSeriesDetails = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}tv/${id}?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+
+export const getSeriesCredits = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}tv/${id}/credits?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+export const getSeriesSimilar = async (id) => {
+  try{
+    const response = await fetch(
+        `${baseUrl}tv/${id}/similar?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}`
+    )
+    const data = await response.json()
+    return data
+  }catch (e) {
+    throw e
+  }
+}
+export const getSearchResult = async(text,category)=>{
+  try{
+    const response  = await fetch(`${baseUrl}search/${category}?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}&query=${text}`)
+    const data = await response.json()
+    return data
+  }
+  catch(e){
+    throw e
+  }
+}
