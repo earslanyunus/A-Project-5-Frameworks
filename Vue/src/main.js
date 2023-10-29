b0ea2960b8d84087200fae6daabd0927d47f0553
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-10-30 00:07:36 +0300

    series part is finished

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -6,11 +6,13 @@ import App from './App.vue'
 import Home from '../src/views/Home.vue'
 import DetailMovie from '../src/views/DetailMovie.vue'
 import DetailPerson from '../src/views/DetailPerson.vue'
+import DetailSeries from '../src/views/DetailSeries.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/detail/movie/:id', component:DetailMovie  },
     { path: '/detail/person/:id', component:DetailPerson  },
+    { path: '/detail/tv/:id', component:DetailSeries  },
 
   ]
 
